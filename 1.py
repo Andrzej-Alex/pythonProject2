b05ea8eb2924d03de8fb462dd5d59d206a66230d
Andrzej-Alex <79633226+Andrzej-Alex@users.noreply.github.com>
2023-05-02 08:20:45 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,12 +1,12 @@
 # 1)
-
+"""
 teskt = "Emma i ja dostałyśmy instrukcje, by o 9:30 napisać o oficjalnych danych dotyczących zatrudnienia w Wielkiej Brytanii i wysłać nasze wersje do redaktora. Byłam przekonana, że Emma będzie ode mnie szybsza, ale miałam też szczerą nadzieję, że to ja będę lepsza. Twórca Emmy, start-up o nazwie Stealth, nazywa ją „niezależną sztuczną inteligencją” zaprojektowaną, by świadczyć profesjonalne usługi, takie jak badania i analiza. Odkąd w modzie są prognozy, że sztuczna inteligencja (ang. artificial intelligence, AI) zastąpi pracowników biurowych, w tym również dziennikarzy, chciałam to sprawdzić na własnej skórze. Emma rzeczywiście była szybka – dane wysłała w 12 minut. Mi zajęło to 35. Jej wersja była też lepsza, niż się spodziewałam. Fakty się zgadzały, zawarła nawet trafne treści, takie jak możliwość Brexitu (choć podzielała wątpliwą opinię, że wyjście z Unii Europejskiej byłoby niezwykle korzystne dla brytyjskiej gospodarki)."
 print("a: ",teskt.count("Emma"))
 print("b: ",teskt.upper())
 print("c: ",teskt.split(' '))
 print("d: ",len(teskt.split('.')))
 print("e: ",teskt.split('.')[0])
-
+"""
 # 2)
 """
 x = int(input("podaj liczbe x: "))
@@ -65,8 +65,8 @@ elif (wybor == "potega"):
     print(kalk1 ** kalk2)
 """
 # 7)
-
+"""
 dowolnyx = int(input("podaj dowolnego x: "))
-
 print((dowolnyx > 1 and dowolnyx < 13))
 print((dowolnyx != 5 or dowolnyx < 0))
+"""
